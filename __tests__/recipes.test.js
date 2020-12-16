750108aa25e8b0490cc0d92cdca462ac172f98cc
Jumpybuns <ethanpiercepresents@gmail.com>
2020-12-15 16:52:55 -0800

    recipe rests passing again

--- __tests__/recipes.test.js
+++ __tests__/recipes.test.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
-const app = require('../lib/app');
 const Recipe = require('../lib/models/recipe');
+const app = require('../lib/app');
+
 
 describe('recipe-lab routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+  afterAll(() => {
+    return pool.end();
   });
 
   it('creates a recipe', () => {
